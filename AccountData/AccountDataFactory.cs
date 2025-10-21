fd44ab153cf2c49d109c28420e2edcd74b0f42b6
ptworek-hash <ptworek@bigtime.net>
2025-10-21 14:32:27 -0500

    Add files via upload

---
+++ AccountData/AccountDataFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace BT.Model.AccountData
+{
+	public static class AccountDataFactory
+	{
+
+		public static IAccountDataRepository Create()
+		{
+			return new AccountDataRepo();
+		}
+	}
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BankAccountApp.Controllers
+{
+	public class HomeController : Controller
+	{
+		// GET: Home
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult Page(String id, string subView = "")
+		{
+			return PartialView($"Page.{id}");
+		}
+
+		[HttpGet]
+        public JsonResult CustomerList()
+        {
+            var repo = new CustomerRepository();
+            var customers = repo.GetList();
+            return Json(customers, JsonRequestBehavior.AllowGet);
+        }
+	}
+}
